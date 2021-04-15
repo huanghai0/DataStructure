3a1ac0e983f90bc33d28ac96ac939d8ca6ed0346
huangh <huangh@bsfit.com.cn>
2021-04-15 16:28:17 +0800

    MarixPrint huangh 04.15 16:28

--- src/print/MaxitPrint.java
+++ src/print/MaxitPrint.java
@@ -1,5 +1,7 @@
 package print;
 
+import java.util.Arrays;
+
 public class MaxitPrint {
 
     public static void main(String[] args) {
@@ -15,7 +17,8 @@ public class MaxitPrint {
                 {9, 10, 11, 12}
         };
 
-        print(arr1);
+        int[] res = print(arr);
+        System.out.println(Arrays.toString(res));
     }
 
     /**
@@ -25,43 +28,51 @@ public class MaxitPrint {
      *
      * @param arr
      */
-    public static void print(int[][] arr) {
+    public static int[] print(int[][] arr) {
         int top = 0;
         int left = 0;
         int right = arr[0].length - 1;
         int botten = arr.length - 1;
 
+        int[] res = new int[arr.length * arr[0].length];
+        int index = 0;
+
         String stat = "right";
         while (left < right && left < right) {
-            if ("right".equals(stat) && left <= right) {
+            if ("right".equals(stat)) {
                 for (int i = left; i <= right; i++) {
                     System.out.print(arr[top][i] + " ");
+                    res[index++] = arr[top][i];
                 }
                 top++;
                 stat = "botten";
                 System.out.println();
-            } else if ("botten".equals(stat) && top <= botten) {
+            } else if ("botten".equals(stat)) {
                 for (int i = top; i <= botten; i++) {
                     System.out.print(arr[i][right] + " ");
+                    res[index++] = arr[i][right];
                 }
                 right--;
                 stat = "left";
                 System.out.println();
-            } else if ("left".equals(stat) && left <= right) {
+            } else if ("left".equals(stat)) {
                 for (int i = right; i >= left; i--) {
                     System.out.print(arr[botten][i] + " ");
+                    res[index++] = arr[botten][i];
                 }
                 botten--;
                 stat = "top";
                 System.out.println();
-            } else if ("top".equals(stat) && top <= botten) {
+            } else if ("top".equals(stat)) {
                 for (int i = botten; i >= top; i--) {
                     System.out.print(arr[i][left] + " ");
+                    res[index++] = arr[i][left];
                 }
                 left++;
                 stat = "right";
                 System.out.println();
             }
         }
+        return res;
     }
 }
